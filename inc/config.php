14e3605cd2fb5bb3d2a6e65ed4e14852d6f0d99b
n3xusddos <31831508+n3xusddos@users.noreply.github.com>
2018-09-07 00:40:41 +0300

    Update and rename  . to inc/config.php

---
+++ inc/config.php
@@ -0,0 +1,6 @@
+<?php
+$av_list = array("avast"=> "waiting","eset" => "waiting","kaspersky" => "waiting","malwarebytes" => "waiting","360totalsecurity" => "waiting","norton" => "waiting","avira" => "waiting","comodo" => "waiting","bitdefender" => "waiting","mcafee" => "waiting","sophos" => "waiting");
+
+ $connection = mysqli_connect( "localhost","root","xxxxxxxx","xxxxxxx");
+
+ ?>
